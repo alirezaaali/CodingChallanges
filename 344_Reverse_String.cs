4b659b9e36c06d09295745818c795ddc6ea07f20
AliReza <alireza.aali@gmail.com>
2022-04-09 15:23:25 -0600

    Update 344_Reverse_String.cs

--- 344_Reverse_String.cs
+++ 344_Reverse_String.cs
@@ -1,5 +1,7 @@
 public class Solution {
     public void ReverseString(char[] s) {
+        
+        // My solution
         int l=0, r=s.Length-1;
         while(l < r){
             if(s[l] != s[r]){
@@ -10,7 +12,16 @@ public class Solution {
             l++;
             r--;
         }
+       
         
+        /* another solution
+         for(int i = 0; i < s.Length / 2; i++){
+            char temp = s[i];
+            s[i] = s[s.Length - 1 - i];
+            s[s.Length - 1 - i] = temp;
+        }
+        */
     }
 }
+
 //https://leetcode.com/problems/reverse-string/

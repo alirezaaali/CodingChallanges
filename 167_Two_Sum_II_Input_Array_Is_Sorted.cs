d2d910d6fbb0390ac35a27f3012f9a26a1f3e950
AliReza <alireza.aali@gmail.com>
2022-04-09 17:15:46 -0600

    Update 167_Two_Sum_II_Input_Array_Is_Sorted.cs
    Better solution

--- 167_Two_Sum_II_Input_Array_Is_Sorted.cs
+++ 167_Two_Sum_II_Input_Array_Is_Sorted.cs
@@ -1,8 +1,8 @@
 public class Solution {
     public int[] TwoSum(int[] numbers, int target) {
-         if(numbers.Length == 0 || numbers.Length < 2)
-          return numbers;
-
+         //if(numbers.Length == 1 || numbers.Length < 2)
+          //return numbers;
+        /* A solution bases on Two Sum
       var hashSet = new HashSet<int>();
       for(var i = 0; i < numbers.Length ;i++){
         var complementary = target- numbers[i];
@@ -11,7 +11,19 @@ public class Solution {
        
         hashSet.Add(numbers[i]);
       }
-     return numbers;
+      */
+        
+        int l = 0, r = numbers.Length-1;
+        while(l < r){
+            var complementary = target - numbers[l];
+            if(numbers[r] == complementary)
+                return new int[]{l+1, r+1};
+            else if(numbers[r] < complementary)
+                l++;
+            else if(numbers[r] > complementary)
+                r--;
+        }
+     return new int[]{l+1, r+1};
         
     }
 }

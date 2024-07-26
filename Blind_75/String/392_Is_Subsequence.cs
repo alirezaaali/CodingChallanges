0a14592c001bf1b3c35cf8b4bd875cf5983e5e16
AliReza <alireza.aali@gmail.com>
2024-07-26 13:30:51 -0600

    392.Is Subsequence

---
+++ Blind_75/String/392_Is_Subsequence.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+	public bool IsSubsequence(string s, string t)
+	{
+		var sp = 0;
+		var tp = 0;
+		while (sp < s.Length && tp < t.Length)
+		{
+			if (s[sp] == t[tp])
+			{
+				sp++;
+			}
+			tp++;
+		}
+		return sp == s.Length;
+	}
+}

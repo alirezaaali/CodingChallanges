23256c91759312463b44bef0117a9d3d0b6768ff
AliReza <alireza.aali@gmail.com>
2024-07-28 19:41:45 -0600

    1395. Count Number of Teams

---
+++ 1395_Count_Number_of_Teams.cs
@@ -0,0 +1,42 @@
+﻿public class Solution
+{
+	public int NumTeams(int[] rating)
+	{
+		var n = rating.Length;
+		var count = 0;
+
+		for (var j = 0; j < n; j++)
+		{
+			var lessBefore = 0;
+			var greaterBefore = 0;
+			var lessAfter = 0;
+			var greaterAfter = 0;
+
+			for (var i = 0; i < j; i++)
+			{
+				if (rating[i] < rating[j])
+				{
+					lessBefore++;
+				}
+				if (rating[i] > rating[j])
+				{
+					greaterBefore++;
+				}
+			}
+
+			for (var k = j + 1; k < n; k++)
+			{
+				if (rating[k] < rating[j])
+				{
+					lessAfter++;
+				}
+				if (rating[k] > rating[j])
+				{
+					greaterAfter++;
+				}
+			}
+			count += lessBefore * greaterAfter + greaterBefore * lessAfter;
+		}
+		return count;
+	}
+}

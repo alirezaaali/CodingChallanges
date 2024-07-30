5eee25b9450691110357cb6a5074dd6fb1641380
AliReza <alireza.aali@gmail.com>
2024-07-29 21:17:39 -0600

    1653. Minimum Deletions to Make String Balanced

---
+++ 1653_Minimum_Deletions_to_Make_String_Balanced.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+	public int MinimumDeletions(string s)
+	{
+		int bCount = 0; // Count of 'b's seen so far
+		int minDeletions = 0; // Minimum deletions needed
+
+		// Traverse the string to find minimum deletions required
+		foreach (char c in s)
+		{
+			if (c == 'b')
+			{
+				bCount++;
+			}
+			else
+			{
+				// When we see an 'a', we can either delete it or delete all 'b's seen so far
+				minDeletions = Math.Min(minDeletions + 1, bCount);
+			}
+		}
+
+		return minDeletions;
+	}
+}

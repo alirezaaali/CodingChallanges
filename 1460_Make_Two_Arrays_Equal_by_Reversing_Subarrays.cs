8f0752942d027b312e5a3372e2bbdcef1c91a91a
AliReza <alireza.aali@gmail.com>
2024-08-02 19:53:15 -0600

    1460. Make Two Arrays Equal by Reversing Subarrays

---
+++ 1460_Make_Two_Arrays_Equal_by_Reversing_Subarrays.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+	public bool CanBeEqual(int[] target, int[] arr)
+	{
+
+		var dict = new Dictionary<int, int>();
+		foreach (var num in target)
+		{
+			if (!dict.Keys.Contains(num))
+			{
+				dict.Add(num, 0);
+			}
+			dict[num]++;
+		}
+		foreach (var num in arr)
+		{
+			if (!dict.Keys.Contains(num) || dict[num] == 0)
+			{
+				return false;
+			}
+			dict[num]--;
+		}
+		return true;
+	}
+}

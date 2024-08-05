d68e749add6f103e0255c1cbc4bf2fd7b022c8c0
AliReza <alireza.aali@gmail.com>
2024-08-05 14:47:13 -0600

    2053. Kth Distinct String in an Array

---
+++ 2053_Kth_Distinct_String_in_an_Array.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+	public string KthDistinct(string[] arr, int k)
+	{
+		var countDict = new Dictionary<string, int>();
+
+		// Count occurrences of each string
+		foreach (var str in arr)
+		{
+			if (countDict.ContainsKey(str))
+			{
+				countDict[str]++;
+			}
+			else
+			{
+				countDict[str] = 1;
+			}
+		}
+
+		// Iterate to find the kth distinct string
+		int distinctCount = 0;
+		foreach (var str in arr)
+		{
+			if (countDict[str] == 1)
+			{
+				distinctCount++;
+				if (distinctCount == k)
+				{
+					return str;
+				}
+			}
+		}
+
+		// If there are fewer than k distinct strings
+		return "";
+	}
+}

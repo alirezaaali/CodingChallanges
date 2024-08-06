6cbbf5b9b924e2fe5cde4f9ffcd9ea841ce3ef70
AliReza <alireza.aali@gmail.com>
2024-08-06 11:54:40 -0600

    169. Majority Element

---
+++ Blind_75/Array/169_Majority_Element.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+	public int MajorityElement(int[] nums)
+	{
+
+		return nums.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key;
+
+		// var acceptedReps = nums.Length/2;
+		// var dict = new Dictionary<int, int>();
+		// foreach(var num in nums){
+		//     if(!dict.ContainsKey(num)){
+		//         dict.Add(num,1);
+		//     }else{
+		//         dict[num]++;
+		//     }
+		// }
+
+		// var result = dict.Where(x => x.Value >= acceptedReps).OrderByDescending(x => x.Value).First().Key;
+
+		// return result;
+	}
+}

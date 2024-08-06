cae0ae19387c220929fefaa9d5fe9758313697fc
AliReza <alireza.aali@gmail.com>
2024-08-06 11:39:39 -0600

    3016. Minimum Number of Pushes to Type Word II

---
+++ 3016_Minimum_Number_of_Pushes_to_Type_Word_II.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+	public int MinimumPushes(string word)
+	{
+		var dict = new Dictionary<char, int>();
+		var result = 0;
+
+		foreach (var c in word)
+		{
+			if (!dict.ContainsKey(c))
+			{
+				dict.Add(c, 1);
+			}
+			else
+			{
+				dict[c]++;
+			}
+		}
+
+		var reps = dict.Select(x => x.Value).OrderByDescending(x => x).ToArray();
+		for (int i = 0; i < reps.Length; i++)
+		{
+			result += reps[i] * (1 + (i / 8));
+		}
+
+		return result;
+	}
+}
